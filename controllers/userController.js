da238e56007610515906b014fd5ea5fa9bbfb311
DESKTOP-UUIAGJN\dell <abhishektyagi6230@gmail.com>
2023-06-07 00:23:00 +0530

    fix:fixing new changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,32 @@
+const fetch = require('node-fetch');
+const http = require('https');
+
 const register =async function(req,res)
 {
-    const {phone,password}=req.body;
-    if(phone=="" || phone == undefined)
-    {
-        return res.send({
-            status_code:true,
-            message:"phone number required"
-        })
-    }
-    res.send({
-        status_code:true,
-        message:"1234"
-    })
+    // let mobile = '917310042077';
+    // let template_idMsg='647f75b1d6fc052fda462523';
+    // let authkeyMsg91='398284AQ1EfNFW647f7502P1';
+    // var otp = 1235;
+    // var phone = `91${mobile}`;
+  
+    // var path=`https://api.msg91.com/api/v5/otp?template_id=${template_idMsg}&mobile=${phone}&authkey=${authkeyMsg91}&otp=${otp}`;
+
+    // const options = { method: 'GET', headers: { accept: 'application/json' } , hostname: 'api.msg91.com' };
+    // await fetch(path, options)
+    //     .then((res) => res.json())
+    //     .then((json) => {
+    //        console.log(json)
+    //     })
+
+    return res.send({
+        status: true,
+        message:"OTP sent"
+    });
 }
+
+// const verifyOtp =async function(req,res)
+// {
+
+// }
 
 module.exports={register}
