e82c892589c6f73efb011944a4aa841bb43a2a6e
Rachit Rawat <rachitthakur311@gmail.com>
2023-06-12 00:31:15 +0530

    talk pandit schema

---
+++ models/talkPandit.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+
+const talkPanditSchema = mngoose.schema({
+category:[{ type:String, is_called: Boolean, }],
+},{ timestamps: true })
+
+const panditTalk = mongoose.model('panditTalk', talkPanditSchema);
+
+module.exports = talkPanditSchema;
