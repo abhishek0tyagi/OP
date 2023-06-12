b9b9f27bbb61b1ed4b597dfc71a657edbce850cc
Deepanshu Tyagi <60540237+Deepanshu09Tyagi@users.noreply.github.com>
2023-06-12 23:49:38 +0530

    added comments

--- controllers/probandsol.js
+++ controllers/probandsol.js
@@ -1,5 +1,11 @@
+//npm
+
+
+//requires
 const probandsol = require('../models/probandsolSchema');
 
+
+//sdk setup
 const PandS = async (req, res) => {
     const { problemCategoryId, problem, description, comment } = req.body;
     await probandsol.create({ problemCategoryId, problem, description, comment, });
