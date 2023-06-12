483b94212ecc190c1497b729a0a5eb78aef04600
Deepanshu Tyagi <60540237+Deepanshu09Tyagi@users.noreply.github.com>
2023-06-13 00:27:11 +0530

    resolve route remove

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,11 @@ const express = require('express');
 const router = express.Router();
 // const { errorHandler } = require('../utils/common');
 // const { rateLimiter5, rateLimiter100 } = require('../middleware/rate-limiter');
-const userController = require('../controllers/userController')
+const userController = require('../controllers/userController');
+const probandsol = require('../controllers/probandsol');
 
 router.get('/register/:phone', userController.register);
 router.post('/verifyPhoneOtp', userController.verifyPhoneOtp);
+router.post('/PandS', probandsol.PandS);
 
 module.exports = router;
